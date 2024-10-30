07f32a4daf3c3f7321d893710581b3a70f5f93a5
rianaditro <rianaditro@gmail.com>
2024-10-30 14:01:04 +0700

    add json parser

---
+++ main.py
@@ -0,0 +1,5 @@
+from scraper import json_parser
+
+
+if __name__ == "__main__":
+    developer_profile = json_parser.reformat_data('DATA.json')
---
+++ scraper/json_parser.py
@@ -0,0 +1,30 @@
+import json
+
+
+
+def reformat_data(json_file:str)->list[dict]:
+    """
+    Parse data from provided JSON file
+    """
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        data = data["data"]
+
+    BASE_URL = "https://www.btnproperti.co.id/"
+    API_PROJECT_DEVELOPER = f"{BASE_URL}api-partners/open-api/v1/manstok/getProperByDevId"
+
+    # Parse developer profile
+    developer_profile = []
+
+    for developer in data:
+        developer_url = f"{BASE_URL}developer/detail/{developer['id'].lower()}"
+        property_url = f"{API_PROJECT_DEVELOPER}?dev_id={developer['id']}&Page=1&Limit=50"
+
+        # Add URL for scraping
+        developer['developer_url'] = developer_url
+        developer['property_url'] = property_url
+
+        developer_profile.append(developer)
+        
+    print("File successfully parsed")
+    return developer_profile 
